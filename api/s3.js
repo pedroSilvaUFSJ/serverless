31b670d60fa42a30a7500b11354f978c8b4efecc
Pedro Henrique da Silva <pedro.silva@bluepanda.pt>
2021-07-06 12:51:05 +0100

    including .png as suffix for the image name

--- api/s3.js
+++ api/s3.js
@@ -15,7 +15,7 @@ const s3 = new aws.S3(s3Object);
 module.exports.generateploadUrl = async function () {
     const rawBytes = await crypto.randomBytes(16);
     const imageName = rawBytes.toString('hex');
-    const params = { Bucket, Key: imageName, Expires: 60 };
+    const params = { Bucket, Key: `${imageName}.png`, Expires: 60 };
     const uploadURL = await s3.getSignedUrlPromise('putObject', params);
     return uploadURL;
 }
--- serverless/imageFunction.js
+++ serverless/imageFunction.js
@@ -4,6 +4,6 @@ module.exports.handle = (event) => {
   event.Records.forEach((record) => {
     const filename = record.s3.object.key;
     const filesize = record.s3.object.size;
-    console.log(`New .png object has been created: ${filename} (${filesize} bytes)`);
+    console.log(`new .png object has been created: ${filename} (${filesize} bytes)`);
   });
 };
